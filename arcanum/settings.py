6b5bea12e3d8aadc1360f95930f36d233fadaee1
john revo <arcanumzxcvbnmm@gmail.com>
2025-06-23 20:52:42 +0700

    Penyesuaian api

--- arcanum/settings.py
+++ arcanum/settings.py
@@ -51,10 +51,15 @@ WSGI_APPLICATION = 'arcanum.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'db_arcanum',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
+
 
 LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
--- brandcloth/models.py
+++ brandcloth/models.py
@@ -28,7 +28,7 @@ class Journal(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     image = models.ImageField(upload_to='journal/')
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True,blank=True)
 
     def __str__(self):
         return self.title
